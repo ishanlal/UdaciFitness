06cc350dffff1982a80df1162f9d8dde49e6c723
Ishan Lal <ishanlal@gmail.com>
2021-05-10 09:21:30 -0700

    removing comments

--- App.js
+++ App.js
@@ -73,59 +73,6 @@ function Tabs() {
 
 const Stack = createStackNavigator();
 
-/*
-const Tabs = TabNavigator({
-  History: {
-    screen: History,
-    navigationOptions: {
-      tabBarLabel: 'History',
-      tabBarIcon: ({ tintcolor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
-    }
-  },
-  AddEntry: {
-    screen: AddEntry,
-    navigationOptions: {
-      tabBarLabel: 'Add Entry',
-      tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
-    }
-  },
-  {
-    navigationOptions: {
-      header: null
-    },
-    tabBarOptions: {
-      activeTintColor: Platform.OS === 'ios' ? purple: white,
-      style: {
-        height: 56,
-        backgroundColor: Platform.OS === 'ios' ? white: purple,
-        shadowColor: 'rgba(0, 0, 0, 0.24)',
-        shadowOffset: {
-          width: 0,
-          height: 3
-        },
-        shadowRadius: 6,
-        shadowOpacity: 1
-      }
-    }
-  }
-})
-
-const MainNavigator = StackNavigator({
-  Home: {
-    screen: Tabs,
-  },
-  EntryDetail: {
-    screen: EntryDetail,
-    navigationOptions: {
-      headerTintColor: white,
-      headerStyle: {
-        backgroundColor: purple,
-      }
-    }
-  }
-})
-*/
-
 export default class App extends React.Component {
   componentDidMount() {
     setLocalNotification()
--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -9,17 +9,6 @@ import {timeToString, getDailyReminderValue} from '../utils/helpers';
 import TextButton from './TextButton';
 
 class EntryDetail extends Component {
-  /*static navigationOptions = ({ navigation }) => {
-    const { entryId } = navigation.state.params
-
-    const year = entryId.slice(0,4)
-    const month = entryId.slice(5,7)
-    const day = entryId.slice(8)
-
-    return {
-      title: `${month}/${day}/${year}`
-    }
-  }*/
 
   reset = () => {
     const { remove, goBack, entryId } = this.props;
